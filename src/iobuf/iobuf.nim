295ca58af9af61d8794b60f4ea77e651bfba4eaa
haoyu <164346864@qq.com>
2023-12-16 19:55:04 +0800

    修改内部对象命名

--- src/iobuf/iobuf.nim
+++ src/iobuf/iobuf.nim
@@ -6,70 +6,70 @@ import intern/iobuf
 import slice2
 
 type
-  IOBuf* = distinct InternIOBuf
+  IOBuf* = distinct InternalIOBuf
 
 proc initBuf*(buf: var IOBuf) {.inline.} =
-  InternIOBuf(buf).initBuf
+  InternalIOBuf(buf).initBuf
 
 proc len*(buf: IOBuf): int {.inline.} =
-  InternIOBuf(buf).len
+  InternalIOBuf(buf).len
 
 proc clear*(buf: var IOBuf) {.inline.} =
-  InternIOBuf(buf).clear
+  InternalIOBuf(buf).clear
 
 proc toSeq*(buf: IOBuf): seq[byte] {.inline.} =
   result = newSeqOfCap[byte](buf.len)
 
-  for region in InternIOBuf(buf):
+  for region in InternalIOBuf(buf):
     result.add(region.toOpenArray())
 
 iterator items*(buf: IOBuf): Slice2[byte] {.inline.} =
-  for region in InternIOBuf(buf):
+  for region in InternalIOBuf(buf):
     yield region.toOpenArray().slice()
 
 proc writeCopy*(buf: var IOBuf, data: pointer, len: int) {.inline.} =
-  InternIOBuf(buf).enqueueRightCopy(data, len)
+  InternalIOBuf(buf).enqueueRightCopy(data, len)
 
 proc writeCopy*(buf: var IOBuf, data: openArray[byte]) {.inline.} =
-  InternIOBuf(buf).enqueueRightCopy(data[0].getAddr, data.len)
+  InternalIOBuf(buf).enqueueRightCopy(data[0].getAddr, data.len)
 
 proc writeCopy*(buf: var IOBuf, data: Slice2[byte]) {.inline.} =
-  InternIOBuf(buf).enqueueRightCopy(data.leftAddr, data.len)
+  InternalIOBuf(buf).enqueueRightCopy(data.leftAddr, data.len)
 
 proc writeCopy*(buf: var IOBuf, data: byte) {.inline.} =
-  InternIOBuf(buf).enqueueByteRight(data)
+  InternalIOBuf(buf).enqueueByteRight(data)
 
 proc writeZeroCopy*(buf: var IOBuf, data: pointer, len: int) {.inline.} =
   var chunk = newChunk(data, len, len)
   var region = initRegion(move chunk, 0, len)
-  InternIOBuf(buf).enqueueRightZeroCopy(move region)
+  InternalIOBuf(buf).enqueueRightZeroCopy(move region)
 
 proc writeZeroCopy*(buf: var IOBuf, data: sink seq[byte]) {.inline.} =
   let len = data.len
   var chunk = newChunk(move data)
   var region = initRegion(move chunk, 0, len)
-  InternIOBuf(buf).enqueueRightZeroCopy(move region)
+  InternalIOBuf(buf).enqueueRightZeroCopy(move region)
 
 proc writeZeroCopy*(buf: var IOBuf, data: IOBuf) {.inline.} =
-  InternIOBuf(buf).enqueueRightZeroCopy(InternIOBuf(data))
+  InternalIOBuf(buf).enqueueRightZeroCopy(InternalIOBuf(data))
 
 proc writeZeroCopy*(buf: var IOBuf, data: IOBuf, size: int) {.inline.} =
   let minSize = min(size, data.len)
 
-  for region in InternIOBuf(buf).visitLeftRegion(minSize):
-    InternIOBuf(buf).enqueueRightZeroCopy(region)
+  for region in InternalIOBuf(buf).visitLeftRegion(minSize):
+    InternalIOBuf(buf).enqueueRightZeroCopy(region)
 
 proc discardBytes*(buf: var IOBuf, size: int) {.inline.} =
   if buf.len <= 0 or size <= 0:
     return
 
-  InternIOBuf(buf).dequeueLeft(size)
+  InternalIOBuf(buf).dequeueLeft(size)
 
 proc peekCopy*(buf: IOBuf, data: pointer, size: int): int {.inline.} =
   if size <= 0 or buf.len <= 0:
     return
 
-  for region in InternIOBuf(buf).visitLeftRegion(size):
+  for region in InternalIOBuf(buf).visitLeftRegion(size):
     let dstAddr = cast[uint](data) + uint(result)
     inc result, region.len
 
@@ -79,7 +79,7 @@ proc peekCopy*(buf: IOBuf, data: var seq[byte], size: int): int {.inline.} =
   if size <= 0 or buf.len <= 0:
     return
 
-  for region in InternIOBuf(buf).visitLeftRegion(size):
+  for region in InternalIOBuf(buf).visitLeftRegion(size):
     data.add(region.toOpenArray)
     inc result, region.len
 
@@ -87,15 +87,15 @@ proc peekZeroCopy*(buf: IOBuf, into: var IOBuf, size: int): int {.inline.} =
   if size <= 0 or buf.len <= 0:
     return
 
-  for region in InternIOBuf(buf).visitLeftRegion(size):
-    InternIOBuf(into).enqueueRightZeroCopy(region)
+  for region in InternalIOBuf(buf).visitLeftRegion(size):
+    InternalIOBuf(into).enqueueRightZeroCopy(region)
     inc result, region.len
 
 proc readCopy*(buf: var IOBuf, data: pointer, size: int): int {.inline.} =
   if size <= 0 or buf.len <= 0:
     return
 
-  for region in InternIOBuf(buf).visitLeftRegionAndDequeue(size):
+  for region in InternalIOBuf(buf).visitLeftRegionAndDequeue(size):
     let dstAddr = cast[uint](data) + uint(result)
     inc result, region.len
 
@@ -106,7 +106,7 @@ proc readCopy*(buf: var IOBuf, data: var seq[byte],
   if size <= 0 or buf.len <= 0:
     return
 
-  for region in InternIOBuf(buf).visitLeftRegionAndDequeue(size):
+  for region in InternalIOBuf(buf).visitLeftRegionAndDequeue(size):
     data.add(region.toOpenArray)
     inc result, region.len
 
@@ -114,6 +114,6 @@ proc readZeroCopy*(buf, into: var IOBuf, size: int): int {.inline.} =
   if size <= 0 or buf.len <= 0:
     return
 
-  for region in InternIOBuf(buf).visitLeftRegionAndDequeue(size):
-    InternIOBuf(into).enqueueRightZeroCopy(region)
+  for region in InternalIOBuf(buf).visitLeftRegionAndDequeue(size):
+    InternalIOBuf(into).enqueueRightZeroCopy(region)
     inc result, region.len
